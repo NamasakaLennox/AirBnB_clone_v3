771d40ab54560c78e61a8174947dc96ae64cf225
NamasakaLennox <namasakalennox254@gmail.com>
2023-10-02 17:53:17 +0300

    fix for docstrings

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-from models import storage
-from models.user import User
+"""
+Handles path for user route
+"""
 from api.v1.views import app_views
 from flask import jsonify, abort, request
+from models import storage
+from models.user import User
 
 
 @app_views.route('/users', methods=['GET'],
