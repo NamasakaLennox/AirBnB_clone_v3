2a9a36462c5bfdfa54517e15fb5ae2f0503bafc2
NamasakaLennox <namasakalennox254@gmail.com>
2023-10-02 18:52:45 +0300

    security improvement- hash passwords using md5

--- fred_test/api/v1/views/states.py
+++ fred_test/api/v1/views/states.py
@@ -5,8 +5,8 @@
         - /states (GET, POST)
         - /states/<state_id> (GET, DELETE, PUT)
 """
-from flask import jsonify, abort, request
 from api.v1.views import app_views
+from flask import jsonify, abort, request
 from models import storage
 from models.state import State
 
