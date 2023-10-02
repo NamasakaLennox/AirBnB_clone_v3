09160ea7e08ee3de8f118c986d162a97e55fbe2c
NamasakaLennox <namasakalennox254@gmail.com>
2023-10-02 15:19:49 +0300

    merge

---
+++ web_static/fred_test/api/v1/views/users.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+"""This module creates a new view for User objects that handles all default
+    RestFul API actions.
+    Routes (api/v1/):
+    -----------------
+        - /users (GET, POST)
+        - /users/<user_id> (GET, DELETE, PUT)
+        - /users/<user_id>/cities (GET, POST)
+"""
+from flask import jsonify, abort, request
+from api.v1.views import app_views
+from models import storage
+from models.user import User
+
+
+@app_views.route('/users', strict_slashes=False, methods=['GET'])
+def all_users():
+    """This function returns a JSON response."""
+    users = [user.to_dict() for user in storage.all(User).values()]
+    return jsonify(users)
+
+@app_views.route('/users/<user_id>', strict_slashes=False,
+                 methods=['GET'])
+def get_user(user_id):
+    """function to retrieve an User object with the id linked to it."""
+    user = storage.get(User, user_id)
+    if user is None:
+        abort(404)
+    return jsonify(user.to_dict())
+
+@app_views.route('/users/<user_id>', strict_slashes=False,
+                 methods=['DELETE'])
+def delete_user(user_id):
+    """This function deletes a User object."""
+    user = storage.get(User, user_id)
+    if user is None:
+        abort(404)
+    storage.delete(user)
+    storage.save()
+    return jsonify({}), 200
+
+@app_views.route('/users', strict_slashes=False, methods=['POST'])
+def post_user():
+    """This function creates a User."""
+    if not request.get_json():
+        abort(400, description="Not a JSON")
+    if 'email' not in request.get_json():
+        abort(400, description="Missing email")
+    if 'password' not in request.get_json():
+        abort(400, description="Missing password")
+    user = User(**request.get_json())
+    storage.new(user)
+    storage.save()
+    return jsonify(user.to_dict()), 201
+
+@app_views.route('/users/<user_id>', strict_slashes=False,
+                 methods=['PUT'])
+def update_user(user_id):
+    """This function updates a User object."""
+    user = storage.get(User, user_id)
+    if user is None:
+        abort(404)
+    if not request.get_json():
+        abort(400, description="Not a JSON")
+    for key, value in request.get_json().items():
+        if key not in ['id', 'email', 'created_at', 'updated_at']:
+            setattr(user, key, value)
+    storage.save()
+    return jsonify(user.to_dict()), 200
